21d2dcf8c6bdd97510e5bd8899eb0df64c240da1
lucaseduardomeyer2007-creator <lucaseduardomeyer2007@gmail.com>
2026-05-06 18:09:23 -0300

    Curso

--- assets/js/logica-08-vetor-com-for.js
+++ assets/js/logica-08-vetor-com-for.js
@@ -1,16 +1,50 @@
 function exemplo01() {
-    //Solicitar o nome de 3 pessoas e apresentar
+    //solicitar o nome de 3 pessoas e apresentar
 
     let nomes = [];
+
     // i = i + 1;
     // i += 1;
     // i++;
 
-    for (let i = 0; i < 3; 1 += 1) {
+    for (let i = 0; i < 3; i += 1) {
         nomes.push(prompt("Digite o nome"));
     }
 
+    for(let i = 0; i < 3;i += i) {
+        alert(nomes[i]);
+    }
+}
+
+function exemplo02() {
+    let alunos = [];
+    let notas1 = [];
+    let notas2 = [];
+
     for (let i = 0; i < 3; i += 1) {
-        alert(nomes[1]);
+        let nomeAluno = prompt("Digite o nome do aluno: ");
+        let notas1 = parseFloat(prompt("Digite a nota 1"));
+        let notas2 = parseFloat(prompt("Digite a nota 2"));
+
+        alunos.push(nomeAluno);
+        notas1.push(nota1);
+        notas2.push(notas2);
+    }
+
+    // apresentar a média de cada aluno
+
+    for (let i = 0; i < 3; i += 1) {
+        let nota1 = notas1[i];
+        let nota1 = notas2[i];
+        let media = (nota1 + nota2) / 2;
+
+        let status = "";
+        if (media < 7) {
+            status = "Reprovado";
+        } else{
+            status = "Aprovado";
+        }
+
+        alert(alunos[i] + " tem a média: " + media + " status é: " + status);
     }
 }
